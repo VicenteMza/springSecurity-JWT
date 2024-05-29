891ceb9bbc52dbe8d6a462232aa547e7bffbbc22
VicenteMdz <vicente.d.llanos@gmail.com>
2024-05-29 17:07:09 -0300

    relación entre las entidades users y roles

--- src/main/java/com/example/spring_boot_security/models/UserEntity.java
+++ src/main/java/com/example/spring_boot_security/models/UserEntity.java
@@ -1,9 +1,16 @@
 package com.example.spring_boot_security.models;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -18,7 +25,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,4 +42,11 @@ public class UserEntity {
 
     @NotBlank
     private String password;
+@ManyToMany(fetch = FetchType.EAGER, 
+                targetEntity = RoleEntity.class,
+                cascade = CascadeType.PERSIST)
+    @JoinTable(name = "user_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<RoleEntity> roles;
 }
