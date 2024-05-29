30d5d9855fc646e25a2e01ef7c2a24d3d9eb48ae
VicenteMdz <vicente.d.llanos@gmail.com>
2024-05-29 16:50:54 -0300

    creacion de entidades para ingreso en springSecurity

---
+++ src/main/java/com/example/spring_boot_security/models/ERole.java
@@ -0,0 +1,7 @@
+package com.example.spring_boot_security.models;
+
+public enum ERole {
+    ADMIN,
+    USER,
+    INVITED
+}
---
+++ src/main/java/com/example/spring_boot_security/models/RoleEntity.java
@@ -0,0 +1,28 @@
+package com.example.spring_boot_security.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "roles")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ERole name;
+}
